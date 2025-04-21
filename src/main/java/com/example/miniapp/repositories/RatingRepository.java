4dbad3c8e161ebca5d6b18cd5272c9973e397734
elazazymohamed <mohamed.elazazyahmed@outlook.com>
2025-04-21 19:23:25 +0200

    Add Rating Model, Repo, Service and Controller

--- src/main/java/com/example/miniapp/repositories/RatingRepository.java
+++ src/main/java/com/example/miniapp/repositories/RatingRepository.java
@@ -1,4 +1,12 @@
 package com.example.miniapp.repositories;
 
-public class RatingRepository {
+import com.example.miniapp.models.Rating;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface RatingRepository extends MongoRepository<Rating, String> {
+    List<Rating> findRatingsByEntityIdAndEntityType(Long entityId, String entityType);
+    List<Rating> findRatingsByScoreGreaterThanEqual(Integer score);
 }
