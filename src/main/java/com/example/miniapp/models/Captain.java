f54e1b1dc981dc488fdeb778dd8fa1772dd629f1
Ismailehab <146727188+Ismailehab@users.noreply.github.com>
2025-04-21 19:06:07 +0200

    Update Captain.java

--- src/main/java/com/example/miniapp/models/Captain.java
+++ src/main/java/com/example/miniapp/models/Captain.java
@@ -1,9 +1,89 @@
 package com.example.miniapp.models;
 import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
-@Table
+@Table(name = "captain")
 public class Captain {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+    @Column(unique = true)
+    private String licenseNumber;
+    private Double avgRatingScore;
+
+    @OneToMany(mappedBy = "captain", cascade = CascadeType.ALL)
+    private List<Trip> trips;
+
+    // Constructors
+    public Captain() {
+
+    }
+
+    public Captain(String name, String licenseNumber, Double avgRatingScore) {
+        this.name = name;
+        this.licenseNumber = licenseNumber;
+        this.avgRatingScore = avgRatingScore;
+        this.trips = new ArrayList<>();
+    }
+
+    public Captain(String name, String licenseNumber, Double avgRatingScore, List<Trip> trips) {
+        this.name = name;
+        this.licenseNumber = licenseNumber;
+        this.avgRatingScore = avgRatingScore;
+        this.trips = trips;
+    }
+
+    public Captain(Long id, String name, String licenseNumber, Double avgRatingScore) {
+        this.id = id;
+        this.name = name;
+        this.licenseNumber = licenseNumber;
+        this.avgRatingScore = avgRatingScore;
+        this.trips = new ArrayList<>();
+    }
+
+    public Captain(Long id, String name, String licenseNumber, Double avgRatingScore, List<Trip> trips) {
+        this.id = id;
+        this.name = name;
+        this.licenseNumber = licenseNumber;
+        this.avgRatingScore = avgRatingScore;
+        this.trips = trips;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public Double getAvgRatingScore() {
+        return avgRatingScore;
+    }
+
+    public void setAvgRatingScore(Double avgRatingScore) {
+        this.avgRatingScore = avgRatingScore;
+    }
 }
